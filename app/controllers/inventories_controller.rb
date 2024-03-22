06c70b54412405305fbdd31d06cf28f8989a62b7
Fombi Favour <fombifavour@gmail.com>
2024-03-22 02:25:40 +0100

    Update inventories_controller.rb

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -8,4 +8,23 @@ class InventoriesController < ApplicationController
     @foods = @inventory.inventory_foods.includes(:food)
     @food = Food.all
   end
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = Inventory.new(inventory_params)
+    if @inventory.save
+      redirect_to inventories_path, notice: 'Inventory created successfully'
+    else
+      render :new, notice: 'Inventory not created'
+    end
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name, :description)
+  end
 end
