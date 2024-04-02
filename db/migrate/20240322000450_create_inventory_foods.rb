7e75c00d804b2a7891109d39e45c81230db76135
Fombi Favour <fombifavour@gmail.com>
2024-04-02 02:39:24 +0100

    Update 20240322000450_create_inventory_foods.rb

--- db/migrate/20240322000450_create_inventory_foods.rb
+++ db/migrate/20240322000450_create_inventory_foods.rb
@@ -2,8 +2,8 @@ class CreateInventoryFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :inventory_foods do |t|
       t.decimal :quantity
-      t.references :inventories, foreign_key: { to_table: :inventories }
-      t.references :foods, foreign_key: { to_table: :foods }
+      t.references :inventory, foreign_key: { to_table: :inventories }
+      t.references :food, foreign_key: { to_table: :foods }
 
       t.timestamps
     end
