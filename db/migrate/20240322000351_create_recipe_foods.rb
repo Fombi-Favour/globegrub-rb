cb6af70f1f381e725948cad30f4e39b31ae7107d
Fombi Favour <fombifavour@gmail.com>
2024-04-02 02:39:28 +0100

    Update 20240322000351_create_recipe_foods.rb

--- db/migrate/20240322000351_create_recipe_foods.rb
+++ db/migrate/20240322000351_create_recipe_foods.rb
@@ -2,8 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :recipe_foods do |t|
       t.decimal :quantity
-      t.references :recipes, foreign_key: { to_table: :recipes }
-      t.references :foods, foreign_key: { to_table: :foods }
+      t.references :recipe, foreign_key: { to_table: :recipes }
+      t.references :food, foreign_key: { to_table: :foods }
 
       t.timestamps
     end
